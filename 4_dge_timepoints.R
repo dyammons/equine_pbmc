cb761f49ef18d755dd293cee258f655b6f2d3b75
dyammons <dyammons95@gmail.com>
2024-09-13 08:38:10 -0600

    Add hclus and plotting overtime to ID GEX trends
    Need to load the exported clades into the scrna data to compelte enrichment
    scoring to try to link the two datasets.

--- 4_dge_timepoints.R
+++ 4_dge_timepoints.R
@@ -8,7 +8,7 @@ outName <- "dge"
 
 ### Run 
 ## (1) Run DEseq2 using LRT
-## (2) Compare to synovial fluid
+## (2) Complete hclus to identify DEG trends
 ## (3) Attempt correlation analysis
 
 
@@ -48,21 +48,64 @@ write.csv(
   sig_res, quote = FALSE, row.names = FALSE,
   file = paste0("../output/", outName, "/all_genes.csv")
 )
-#Heatmap of DEGs
+
+### (2) Complete hclus to identify DEG trends
 mat <- counts(dds, normalized = TRUE)[sig_res$gene, ]
-ht <- Heatmap(t(scale(t(mat))), k = 4, cluster_columns = F)
+mat_scaled <- t(scale(t(mat)))
+M <- (1 - cor(t(mat_scaled), method = "pearson")) / 2
+hc <- hclust(as.dist(M), method = "complete")
+ht <- Heatmap(
+  name = "Scaled\nexpression",
+  mat_scaled, 
+  cluster_columns = F, 
+  show_row_names = F,
+  cluster_rows = F,
+  row_split = cutree(hc, k = 6)
+)
 png(file = paste0("../output/", outName, "/DE_heat.png"), width = 4000, height = 4000, res = 400)
 par(mfcol = c(1, 1))
 draw(ht)
 dev.off()
+#Generate boxplot of average expression at each timepoint
+avg_exp <- as.data.frame(t(mat_scaled)) %>%
+  rownames_to_column() %>%
+  pivot_longer(cols = colnames(.)[2:ncol(.)], names_to = "gene") %>%
+  separate(rowname, remove = F, sep = "_", into = c("horse", "timepoint")) %>%
+  group_by(gene, timepoint) %>%
+  summarize(
+    MEAN = mean(value)
+  )
+avg_exp$timepoint <- factor(
+  avg_exp$timepoint, levels = c("Day0", "Day14", "Day28", "Day56", "Day126")
+)
+#Bring over the clustering results -- could improve on this
+avg_exp <- left_join(
+  avg_exp,
+  rownames_to_column(data.frame(
+    clade = cutree(hc, k = 6)
+  )),
+  by = c("gene" = "rowname")
+)
+#Create the plot
+ggplot(data = avg_exp, aes(x = timepoint, y = MEAN)) +
+  geom_boxplot() +
+  geom_point() +
+  facet_wrap(clade ~ ., nrow = 2)
+ggsave(file = paste0("../output/", outName, "/gex_over_time.png"))
+#Print the genes from the clade that decrease with time
+print(unique(avg_exp[avg_exp$clade == 2, ]$gene))
+#Save the clustering resutlts to complete enrichment scoring w/ scrna data
+write.csv(
+  avg_exp, quote = FALSE, row.names = FALSE,
+  file = paste0("../output/", outName, "/deg_clades.csv")
+)
 
-
-### (2) Compare to synovial fluid -- minimal overlap
+### (3) Compare to synovial fluid -- minimal overlap
 res_sf <- read.csv("../../sf/output/allCells_clean/lrt/All cells/allCells_clean_cluster_All cells_all_genes.csv")
 mat <- mat[intersect(res_sf$gene, sig_res$gene), ]
 
 
-### (3) Attempt correlation analysis
+### (4) Attempt correlation analysis
 lapply(rownames(mat), FUN = function(x){
   browser()
   dat <- data.frame(value = mat[x, ]) %>%
